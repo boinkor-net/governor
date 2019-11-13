32a2cd95140ddfddcc643fe26a01ae1834382e71
Andreas Fuchs <asf@boinkor.net>
2019-11-13 09:18:22 -0500

    Actually panic when given a too-long duration

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,24 @@ use std::{cmp, fmt};
 
 mod clock;
 pub use clock::*;
+use std::convert::TryInto;
 use std::ops::{Add, Sub};
 
 /// A number of nanoseconds from a reference point.
 ///
 /// Can not represent durations >584 years, but hopefully that
-/// should not be a problem in real-world applications
+/// should not be a problem in real-world applications.
 #[derive(Add, Sub, PartialEq, Eq, Default, Debug, From, Into, Clone, Copy, PartialOrd, Ord)]
 struct Nanos(u64);
 
 impl From<Duration> for Nanos {
     fn from(d: Duration) -> Self {
-        Nanos(d.as_nanos() as u64)
+        // This can panic:
+        Nanos(
+            d.as_nanos()
+                .try_into()
+                .expect("Duration is longer than 584 years"),
+        )
     }
 }
 
