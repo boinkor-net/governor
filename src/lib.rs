f44a294ec522fb4c08548432c98ac2361ef2672b
Andreas Fuchs <asf@boinkor.net>
2019-11-13 09:20:15 -0500

    move nanos out

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,11 @@
-use derive_more::*;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Duration;
 use std::{cmp, fmt};
 
 mod clock;
 pub use clock::*;
-use std::convert::TryInto;
-use std::ops::{Add, Sub};
-
-/// A number of nanoseconds from a reference point.
-///
-/// Can not represent durations >584 years, but hopefully that
-/// should not be a problem in real-world applications.
-#[derive(Add, Sub, PartialEq, Eq, Default, Debug, From, Into, Clone, Copy, PartialOrd, Ord)]
-struct Nanos(u64);
-
-impl From<Duration> for Nanos {
-    fn from(d: Duration) -> Self {
-        // This can panic:
-        Nanos(
-            d.as_nanos()
-                .try_into()
-                .expect("Duration is longer than 584 years"),
-        )
-    }
-}
-
-impl Into<Duration> for Nanos {
-    fn into(self) -> Duration {
-        Duration::from_nanos(self.0)
-    }
-}
-
-impl Add<Duration> for Nanos {
-    type Output = Nanos;
-
-    fn add(self, rhs: Duration) -> Self::Output {
-        let rhs: Nanos = rhs.into();
-        Nanos(rhs.0 + self.0)
-    }
-}
-
-impl Sub<Duration> for Nanos {
-    type Output = Nanos;
-
-    fn sub(self, rhs: Duration) -> Self::Output {
-        let rhs: Nanos = rhs.into();
-        Nanos(rhs.0 - self.0)
-    }
-}
-
-impl Nanos {
-    fn saturating_sub(self, rhs: Nanos) -> Nanos {
-        Nanos(self.0.saturating_sub(rhs.0))
-    }
-}
+mod nanos;
+use nanos::Nanos;
 
 #[derive(Default, Debug)]
 pub struct Tat(AtomicU64);
