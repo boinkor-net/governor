78110e5114e2d6833cec5cbc4a8fdd90e28b4e65
Andreas Fuchs <asf@boinkor.net>
2019-11-14 23:17:28 -0500

    Make the benchmarks use the rate limiter with the default clock
    These are somewhat better measurements - closer to the real world, after all

--- benches/multi_threaded.rs
+++ benches/multi_threaded.rs
@@ -1,10 +1,8 @@
 use criterion::{black_box, Benchmark, Criterion, Throughput};
-use governor::clock::FakeRelativeClock;
 use governor::{DirectRateLimiter, Quota};
 use nonzero_ext::*;
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
@@ -13,27 +11,21 @@ pub fn bench_all(c: &mut Criterion) {
 fn bench_direct(c: &mut Criterion) {
     let id = "multi_threaded/direct";
     let bm = Benchmark::new(id, |b| {
-        let mut clock = FakeRelativeClock::new();
-        let ms = Duration::from_millis(20);
         let mut children = vec![];
-        let lim = Arc::new(DirectRateLimiter::new_with_clock(
-            Quota::per_second(nonzero!(5u32)),
-            &clock,
-        ));
+        let lim = Arc::new(DirectRateLimiter::new(Quota::per_second(nonzero!(
+            1_000_000u32
+        ))));
 
         for _i in 0..19 {
             let lim = lim.clone();
-            let mut clock = clock.clone();
             let mut b = *b;
             children.push(thread::spawn(move || {
                 b.iter(|| {
-                    clock.advance(ms);
                     black_box(lim.check().is_ok());
                 });
             }));
         }
         b.iter(|| {
-            clock.advance(ms);
             black_box(lim.check().is_ok());
         });
         for child in children {
--- benches/single_threaded.rs
+++ benches/single_threaded.rs
@@ -1,8 +1,6 @@
 use criterion::{black_box, Benchmark, Criterion, Throughput};
-use governor::clock::FakeRelativeClock;
 use governor::{DirectRateLimiter, Quota};
 use nonzero_ext::*;
-use std::time::Duration;
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
@@ -11,11 +9,8 @@ pub fn bench_all(c: &mut Criterion) {
 fn bench_direct(c: &mut Criterion) {
     let id = "single_threaded/direct";
     let bm = Benchmark::new(id, |b| {
-        let mut clock = FakeRelativeClock::new();
-        let ms = Duration::from_millis(20);
-        let rl = DirectRateLimiter::new_with_clock(Quota::per_second(nonzero!(5u32)), &clock);
+        let rl = DirectRateLimiter::new(Quota::per_second(nonzero!(1_000_000u32)));
         b.iter(|| {
-            clock.advance(ms);
             black_box(rl.check().is_ok());
         });
     })
