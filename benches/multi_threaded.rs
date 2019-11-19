ed3fe5bf532ec547ce7b69bccf30a5e8a3cf9f74
Andreas Fuchs <asf@boinkor.net>
2019-11-18 22:22:01 -0500

    Make benchmarks for keyed rate limiters
    single- and multi-threaded, they are faar slower; but then I'm only looking
    at a single key and the benchmark is also kinda pathological.

--- benches/multi_threaded.rs
+++ benches/multi_threaded.rs
@@ -1,11 +1,14 @@
 use criterion::{black_box, Benchmark, Criterion, Throughput};
-use governor::{Quota, RateLimiter};
+use governor::state::keyed::{DashMapStateStore, HashMapStateStore, KeyedStateStore};
+use governor::{clock, Quota, RateLimiter};
 use nonzero_ext::*;
 use std::sync::Arc;
 use std::thread;
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
+    bench_keyed::<HashMapStateStore<u32>>(c, "hashmap");
+    bench_keyed::<DashMapStateStore<u32>>(c, "dashmap");
 }
 
 fn bench_direct(c: &mut Criterion) {
@@ -34,4 +37,43 @@ fn bench_direct(c: &mut Criterion) {
     })
     .throughput(Throughput::Elements(1));
     c.bench(id, bm);
+}
+
+fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(
+    c: &mut Criterion,
+    name: &str,
+) {
+    let id = format!("multi_threaded/keyed/{}", name);
+    let bm = Benchmark::new(&id, |b| {
+        let mut children = vec![];
+        let state: M = Default::default();
+        let clock = clock::MonotonicClock::default();
+        let lim = Arc::new(RateLimiter::new(
+            Quota::per_second(nonzero!(1_000_000u32)),
+            state,
+            &clock,
+        ));
+
+        for _i in 0..19 {
+            let lim = lim.clone();
+            let mut b = *b;
+            children.push(thread::spawn(move || {
+                b.iter(|| {
+                    black_box(lim.check_key(&1u32).is_ok());
+                    black_box(lim.check_key(&2u32).is_ok());
+                    black_box(lim.check_key(&3u32).is_ok());
+                });
+            }));
+        }
+        b.iter(|| {
+            black_box(lim.check_key(&1u32).is_ok());
+            black_box(lim.check_key(&2u32).is_ok());
+            black_box(lim.check_key(&3u32).is_ok());
+        });
+        for child in children {
+            child.join().unwrap();
+        }
+    })
+    .throughput(Throughput::Elements(3));
+    c.bench(&id, bm);
 }
--- benches/single_threaded.rs
+++ benches/single_threaded.rs
@@ -1,9 +1,12 @@
 use criterion::{black_box, Benchmark, Criterion, Throughput};
-use governor::{Quota, RateLimiter};
+use governor::state::keyed::{DashMapStateStore, HashMapStateStore, KeyedStateStore};
+use governor::{clock, Quota, RateLimiter};
 use nonzero_ext::*;
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
+    bench_keyed::<HashMapStateStore<u32>>(c, "hashmap");
+    bench_keyed::<DashMapStateStore<u32>>(c, "dashmap");
 }
 
 fn bench_direct(c: &mut Criterion) {
@@ -16,4 +19,21 @@ fn bench_direct(c: &mut Criterion) {
     })
     .throughput(Throughput::Elements(1));
     c.bench(id, bm);
+}
+
+fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(
+    c: &mut Criterion,
+    name: &str,
+) {
+    let id = format!("single_threaded/keyed/{}", name);
+    let bm = Benchmark::new(&id, |b| {
+        let state: M = Default::default();
+        let clock = clock::MonotonicClock::default();
+        let rl = RateLimiter::new(Quota::per_second(nonzero!(1_000_000u32)), state, &clock);
+        b.iter(|| {
+            black_box(rl.check_key(&1u32).is_ok());
+        });
+    })
+    .throughput(Throughput::Elements(1));
+    c.bench(&id, bm);
 }
