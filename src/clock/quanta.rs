2e0b5afbb544bfa55553081df2d1263734e198b7
Andreas Fuchs <asf@boinkor.net>
2019-11-22 09:26:35 -0500

    Implement a second quanta clock type for the background thread
    It's more useful to have one that does the right thing by default, and
    one that's more useful for people who can spare a CPU core.

--- src/clock/quanta.rs
+++ src/clock/quanta.rs
@@ -1,31 +1,91 @@
 use crate::lib::*;
 
-use crate::clock::Clock;
+use crate::clock::{Clock, Reference};
 use crate::nanos::Nanos;
-use lazy_static::*;
 use quanta;
 
-/// A clock using the [`quanta`] crate.
+/// A clock using the default [`quanta::Clock`] structure.
 ///
-/// It works by keeping a time keeping thread that updates a reference time every 100ns.
+/// This clock uses [`quanta::Clock.now`], which does retrieve the time synchronously. To use a
+/// clock that uses a quanta background upkeep thread (which allows retrieving the time with an
+/// atomic read, but requires a background thread that wakes up continually),
+/// see [`QuantaUpkeepClock`].
 #[derive(Debug, Clone)]
 pub struct QuantaClock(quanta::Clock);
 
 impl Default for QuantaClock {
     fn default() -> Self {
-        lazy_static! {
-            static ref HANDLE: quanta::Handle = quanta::Builder::new(Duration::from_nanos(100))
-                .start()
-                .expect("should build the reference handle");
-        }
         QuantaClock(Default::default())
     }
 }
 
 impl Clock for QuantaClock {
-    type Instant = Nanos;
+    type Instant = QuantaInstant;
 
     fn now(&self) -> Self::Instant {
-        Nanos::from(self.0.recent())
+        QuantaInstant(Nanos::from(self.0.now()))
+    }
+}
+
+/// A nanosecond-scale opaque instant (already scaled to reference time) returned from a
+/// [`QuantaClock`].
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct QuantaInstant(Nanos);
+
+impl Add<Nanos> for QuantaInstant {
+    type Output = QuantaInstant;
+
+    fn add(self, other: Nanos) -> QuantaInstant {
+        QuantaInstant(self.0 + other)
+    }
+}
+
+impl Reference for QuantaInstant {
+    fn duration_since(&self, earlier: Self) -> Nanos {
+        self.0.duration_since(earlier.0)
+    }
+
+    fn saturating_sub(&self, duration: Nanos) -> Self {
+        QuantaInstant(self.0.saturating_sub(duration))
+    }
+}
+
+/// A clock using the default [`quanta::Clock`] structure and an upkeep thread.
+///
+/// This clock relies on an upkeep thread that wakes up in regular (user defined) intervals to
+/// retrieve the current time and update an atomic U64; the clock then can retrieve that time
+/// (and is as behind as, at most, that interval).
+///
+/// The background thread is stopped as soon as the last clone of the clock is
+/// dropped.
+///
+/// Whether this is faster than a [`QuantaClock`] depends on the utilization of the rate limiter
+/// and the upkeep interval that you pick; you should measure and compare performance before
+/// picking one or the other.
+#[derive(Debug, Clone)]
+pub struct QuantaUpkeepClock(quanta::Clock, Arc<quanta::Handle>);
+
+impl QuantaUpkeepClock {
+    /// Returns a new `QuantaUpkeepClock` with an upkeep thread that wakes up once in `interval`.
+    pub fn from_interval(interval: Duration) -> Result<QuantaUpkeepClock, std::io::Error> {
+        let builder = quanta::Builder::new(interval);
+        Self::from_builder(builder)
+    }
+
+    /// Returns a new `QuantaUpkeepClock` with an upkeep thread as specified by the given builder.
+    pub fn from_builder(builder: quanta::Builder) -> Result<QuantaUpkeepClock, std::io::Error> {
+        let handle = builder.start()?;
+        Ok(QuantaUpkeepClock(
+            quanta::Clock::default(),
+            Arc::new(handle),
+        ))
+    }
+}
+
+impl Clock for QuantaUpkeepClock {
+    type Instant = QuantaInstant;
+
+    fn now(&self) -> Self::Instant {
+        QuantaInstant(Nanos::from(self.0.recent()))
     }
 }
