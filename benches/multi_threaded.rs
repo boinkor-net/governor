8e618215a2f1eb41ff073acc65dab927e1dd604c
Andreas Fuchs <asf@boinkor.net>
2019-11-23 10:41:28 -0500

    Upgrade criterion, add benchmarks for the various rt clocks
    The criterion upgrade brings with itself a bunch of ergonomics/boilerplate
    improvements, but requires that threaded benchmarks are worse: Measuring
    stuff requires manual start/end timing around thread setup/teardown.

--- benches/multi_threaded.rs
+++ benches/multi_threaded.rs
@@ -1,21 +1,24 @@
-use criterion::{black_box, BatchSize, Benchmark, Criterion, Throughput};
+use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 use governor::state::keyed::{DashMapStateStore, HashMapStateStore, KeyedStateStore};
 use governor::{clock, Quota, RateLimiter};
 use nonzero_ext::*;
+use std::any::type_name;
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
-    bench_keyed::<HashMapStateStore<u32>>(c, "hashmap");
-    bench_keyed::<DashMapStateStore<u32>>(c, "dashmap");
+    bench_keyed::<HashMapStateStore<u32>>(c);
+    bench_keyed::<DashMapStateStore<u32>>(c);
 }
 
+const THREADS: u32 = 20;
+
 fn bench_direct(c: &mut Criterion) {
-    let id = "multi_threaded/direct";
-    let bm = Benchmark::new(id, |b| {
-        let mut children = vec![];
+    let mut group = c.benchmark_group("multi_threaded");
+    group.throughput(Throughput::Elements(1));
+    group.bench_function("direct", |b| {
         let ms = Duration::from_millis(1);
         let clock = clock::FakeRelativeClock::default();
         let lim = Arc::new(RateLimiter::direct_with_clock(
@@ -23,83 +26,64 @@ fn bench_direct(c: &mut Criterion) {
             &clock,
         ));
 
-        for _i in 0..19 {
-            let lim = lim.clone();
-            let clock = clock.clone();
-            let mut b = *b;
-            children.push(thread::spawn(move || {
-                b.iter_batched(
-                    || {
+        b.iter_custom(|iters| {
+            let mut children = vec![];
+            let start = Instant::now();
+            for _i in 0..THREADS {
+                let lim = lim.clone();
+                let clock = clock.clone();
+                children.push(thread::spawn(move || {
+                    for _i in 0..iters {
                         clock.advance(ms);
-                    },
-                    |()| {
                         black_box(lim.check().is_ok());
-                    },
-                    BatchSize::SmallInput,
-                );
-            }));
-        }
-        b.iter(|| {
-            clock.advance(ms);
-            black_box(lim.check().is_ok());
-        });
-        for child in children {
-            child.join().unwrap();
-        }
-    })
-    .throughput(Throughput::Elements(1));
-    c.bench(id, bm);
+                    }
+                }));
+            }
+            for child in children {
+                child.join().unwrap()
+            }
+            start.elapsed()
+        })
+    });
+    group.finish();
 }
 
-fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(
-    c: &mut Criterion,
-    name: &str,
-) {
-    let id = format!("multi_threaded/keyed/{}", name);
-    let bm = Benchmark::new(&id, |b| {
-        let mut children = vec![];
-        let state: M = Default::default();
+fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(c: &mut Criterion) {
+    let mut group = c.benchmark_group("multi_threaded");
+
+    // We perform 3 checks per thread per iter:
+    group.throughput(Throughput::Elements(3));
+
+    group.bench_function(BenchmarkId::new("keyed", type_name::<M>()), |b| {
         let ms = Duration::from_millis(1);
         let clock = clock::FakeRelativeClock::default();
+        let state: M = Default::default();
         let lim = Arc::new(RateLimiter::new(
             Quota::per_second(nonzero!(50u32)),
             state,
             &clock,
         ));
 
-        for _i in 0..19 {
-            let lim = lim.clone();
-            let clock = clock.clone();
-            let mut b = *b;
-            children.push(thread::spawn(move || {
-                b.iter_batched(
-                    || {
+        b.iter_custom(|iters| {
+            let mut children = vec![];
+            let start = Instant::now();
+            for _i in 0..THREADS {
+                let lim = lim.clone();
+                let clock = clock.clone();
+                children.push(thread::spawn(move || {
+                    for _i in 0..iters {
                         clock.advance(ms);
-                    },
-                    |()| {
                         black_box(lim.check_key(&1u32).is_ok());
                         black_box(lim.check_key(&2u32).is_ok());
                         black_box(lim.check_key(&3u32).is_ok());
-                    },
-                    BatchSize::SmallInput,
-                );
-            }));
-        }
-        b.iter_batched(
-            || {
-                clock.advance(ms);
-            },
-            |()| {
-                black_box(lim.check_key(&1u32).is_ok());
-                black_box(lim.check_key(&2u32).is_ok());
-                black_box(lim.check_key(&3u32).is_ok());
-            },
-            BatchSize::SmallInput,
-        );
-        for child in children {
-            child.join().unwrap();
-        }
-    })
-    .throughput(Throughput::Elements(3));
-    c.bench(&id, bm);
+                    }
+                }));
+            }
+            for child in children {
+                child.join().unwrap()
+            }
+            start.elapsed()
+        })
+    });
+    group.finish();
 }
--- benches/single_threaded.rs
+++ benches/single_threaded.rs
@@ -1,18 +1,20 @@
-use criterion::{black_box, BatchSize, Benchmark, Criterion, Throughput};
+use criterion::{black_box, BatchSize, BenchmarkId, Criterion, Throughput};
 use governor::state::keyed::{DashMapStateStore, HashMapStateStore, KeyedStateStore};
 use governor::{clock, Quota, RateLimiter};
 use nonzero_ext::*;
+use std::any::type_name;
 use std::time::Duration;
 
 pub fn bench_all(c: &mut Criterion) {
     bench_direct(c);
-    bench_keyed::<HashMapStateStore<u32>>(c, "hashmap");
-    bench_keyed::<DashMapStateStore<u32>>(c, "dashmap");
+    bench_keyed::<HashMapStateStore<u32>>(c);
+    bench_keyed::<DashMapStateStore<u32>>(c);
 }
 
 fn bench_direct(c: &mut Criterion) {
-    let id = "single_threaded/direct";
-    let bm = Benchmark::new(id, |b| {
+    let mut group = c.benchmark_group("single_threaded");
+    group.throughput(Throughput::Elements(1));
+    group.bench_function("direct", |b| {
         let clock = clock::FakeRelativeClock::default();
         let step = Duration::from_millis(20);
         let rl = RateLimiter::direct_with_clock(Quota::per_second(nonzero!(50u32)), &clock);
@@ -25,31 +27,30 @@ fn bench_direct(c: &mut Criterion) {
             },
             BatchSize::SmallInput,
         );
-    })
-    .throughput(Throughput::Elements(1));
-    c.bench(id, bm);
+    });
+    group.finish();
 }
 
-fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(
-    c: &mut Criterion,
-    name: &str,
-) {
-    let id = format!("single_threaded/keyed/{}", name);
-    let bm = Benchmark::new(&id, |b| {
-        let state: M = Default::default();
-        let clock = clock::FakeRelativeClock::default();
-        let step = Duration::from_millis(20);
-        let rl = RateLimiter::new(Quota::per_second(nonzero!(50u32)), state, &clock);
-        b.iter_batched(
-            || {
-                clock.advance(step);
-            },
-            |()| {
-                black_box(rl.check_key(&1u32).is_ok());
-            },
-            BatchSize::SmallInput,
-        );
-    })
-    .throughput(Throughput::Elements(1));
-    c.bench(&id, bm);
+fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(c: &mut Criterion) {
+    let mut group = c.benchmark_group("single_threaded");
+    group.throughput(Throughput::Elements(3));
+
+    group
+        .bench_function(BenchmarkId::new("keyed", type_name::<M>()), |b| {
+            let state: M = Default::default();
+            let clock = clock::FakeRelativeClock::default();
+            let step = Duration::from_millis(20);
+            let rl = RateLimiter::new(Quota::per_second(nonzero!(50u32)), state, &clock);
+            b.iter_batched(
+                || {
+                    clock.advance(step);
+                },
+                |()| {
+                    black_box(rl.check_key(&1u32).is_ok());
+                },
+                BatchSize::SmallInput,
+            );
+        })
+        .throughput(Throughput::Elements(1));
+    group.finish();
 }
