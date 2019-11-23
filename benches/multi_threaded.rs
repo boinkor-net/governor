16036b0e62902c3ebca050ebd982916f4dc757cf
Andreas Fuchs <asf@boinkor.net>
2019-11-23 10:51:31 -0500

    Move setup of the multithreaded limiter into the iter_custom block

--- benches/multi_threaded.rs
+++ benches/multi_threaded.rs
@@ -1,3 +1,7 @@
+//! Benchmarks measuring how long it takes a set of 20 threads to measure `iter` cells each.
+//! The longest-running thread's time is reported. These benchmarks unfortunately measure a certain
+//! amount of overhead in thread setup and teardown.
+
 use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 use governor::state::keyed::{DashMapStateStore, HashMapStateStore, KeyedStateStore};
 use governor::{clock, Quota, RateLimiter};
@@ -19,22 +23,20 @@ fn bench_direct(c: &mut Criterion) {
     let mut group = c.benchmark_group("multi_threaded");
     group.throughput(Throughput::Elements(1));
     group.bench_function("direct", |b| {
-        let ms = Duration::from_millis(1);
-        let clock = clock::FakeRelativeClock::default();
-        let lim = Arc::new(RateLimiter::direct_with_clock(
-            Quota::per_second(nonzero!(50u32)),
-            &clock,
-        ));
+        let clock = clock::QuantaUpkeepClock::from_interval(Duration::from_micros(10))
+            .expect("Could not spawn upkeep thread");
 
         b.iter_custom(|iters| {
+            let lim = Arc::new(RateLimiter::direct_with_clock(
+                Quota::per_second(nonzero!(50u32)),
+                &clock,
+            ));
             let mut children = vec![];
             let start = Instant::now();
             for _i in 0..THREADS {
                 let lim = lim.clone();
-                let clock = clock.clone();
                 children.push(thread::spawn(move || {
                     for _i in 0..iters {
-                        clock.advance(ms);
                         black_box(lim.check().is_ok());
                     }
                 }));
@@ -55,24 +57,23 @@ fn bench_keyed<M: KeyedStateStore<u32> + Default + Send + Sync + 'static>(c: &mu
     group.throughput(Throughput::Elements(3));
 
     group.bench_function(BenchmarkId::new("keyed", type_name::<M>()), |b| {
-        let ms = Duration::from_millis(1);
-        let clock = clock::FakeRelativeClock::default();
-        let state: M = Default::default();
-        let lim = Arc::new(RateLimiter::new(
-            Quota::per_second(nonzero!(50u32)),
-            state,
-            &clock,
-        ));
+        let clock = clock::QuantaUpkeepClock::from_interval(Duration::from_micros(10))
+            .expect("Could not spawn upkeep thread");
 
         b.iter_custom(|iters| {
+            let state: M = Default::default();
+            let lim = Arc::new(RateLimiter::new(
+                Quota::per_second(nonzero!(50u32)),
+                state,
+                &clock,
+            ));
+
             let mut children = vec![];
             let start = Instant::now();
             for _i in 0..THREADS {
                 let lim = lim.clone();
-                let clock = clock.clone();
                 children.push(thread::spawn(move || {
                     for _i in 0..iters {
-                        clock.advance(ms);
                         black_box(lim.check_key(&1u32).is_ok());
                         black_box(lim.check_key(&2u32).is_ok());
                         black_box(lim.check_key(&3u32).is_ok());
