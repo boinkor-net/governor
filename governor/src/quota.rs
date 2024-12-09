boinkor-net/governor

commit 273f77b16db676f7ba0b0f9ecca5fc19586e501f

Differentiate between GCRA burst capacity and tolerance
In GCRA terms, the total capacity of the rate limiter is `Τ + τ`, where
`Τ` is the nominal cell arrival interval and `τ` is (extra) tolerance
allowed for bursts.

The current implementation has a subtle issue, caused by conflating
capacity and tolerance, which results in one extra cell being allowed
after arrival intervals equal or greater than "capacity + 1", as
reported in #107 and #249.

This problem was partially hidden by a compensating addition of Τ to
initial TAT in the case of the first cell (i.e. when the rate limiter
has no state recorded for it).

To fix the issue, correct the value `tau` used for making the decision,
making it the tolerance, and remove the adjustment for the first cell
that's no longer necessary.

Fixes: #107
Fixes: #249

governor/src/quota.rs
@@ -190,15 +190,14 @@ impl Quota {
     /// This is useful mainly for [`crate::middleware::RateLimitingMiddleware`]
     /// where custom code may want to construct information based on
     /// the amount of burst balance remaining.
-    pub(crate) fn from_gcra_parameters(t: Nanos, tau: Nanos) -> Quota {
-        // Safety assurance: As we're calling this from this crate
-        // only, and we do not allow creating a Gcra from 0
-        // parameters, this is, in fact, safe.
+    pub(crate) fn from_gcra_parameters(t: Nanos, capacity: Nanos) -> Quota {
+        // Safety assurance: by construction, the computed value is bounded by
+        // one at the lower.
         //
-        // The casts may look a little sketch, but again, they're
-        // constructed from parameters that came from the crate
-        // exactly like that.
-        let max_burst = unsafe { NonZeroU32::new_unchecked((tau.as_u64() / t.as_u64()) as u32) };
+        // The casts may look a little sketch, but they're constructed from
+        // parameters that came from the crate exactly like that.
+        let max_burst =
+            unsafe { NonZeroU32::new_unchecked(1 + (capacity.as_u64() / t.as_u64()) as u32) };
         let replenish_1_per = t.into();
         Quota {
             max_burst,

governor/src/gcra.rs
@@ -79,22 +79,21 @@ pub(crate) struct Gcra {
     /// The "weight" of a single packet in units of time.
     t: Nanos,
 
-    /// The "burst capacity" of the bucket.
+    /// The "tolerance" of the bucket in units of time.
+    ///
+    /// The total "burst capacity" of the bucket is `t + tau`.
     tau: Nanos,
 }
 
 impl Gcra {
     pub(crate) fn new(quota: Quota) -> Self {
-        let tau: Nanos = (cmp::max(quota.replenish_1_per, Duration::from_nanos(1))
-            * quota.max_burst.get())
-        .into();
-        let t: Nanos = quota.replenish_1_per.into();
+        let t: Nanos = cmp::max(quota.replenish_1_per, Duration::from_nanos(1)).into();
+        let tau: Nanos = t * (quota.max_burst.get() - 1).into();
         Gcra { t, tau }
     }
 
-    /// Computes and returns a new ratelimiter state if none exists yet.
-    fn starting_state(&self, t0: Nanos) -> Nanos {
-        t0 + self.t
+    pub(crate) fn t(&self) -> Nanos {
+        self.t
     }
 
     /// Tests a single cell against the rate limiter state and updates it at the given key.
@@ -114,7 +113,7 @@ impl Gcra {
         let tau = self.tau;
         let t = self.t;
         state.measure_and_replace(key, |tat| {
-            let tat = tat.unwrap_or_else(|| self.starting_state(t0));
+            let tat = tat.unwrap_or(t0);
             let earliest_time = tat.saturating_sub(tau);
             if t0 < earliest_time {
                 Err(MW::disallow(
@@ -154,10 +153,12 @@ impl Gcra {
         // check that we can allow enough cells through. Note that `additional_weight` is the
         // value of the cells *in addition* to the first cell - so add that first cell back.
         if additional_weight + t > tau {
-            return Err(InsufficientCapacity((tau.as_u64() / t.as_u64()) as u32));
+            return Err(InsufficientCapacity(
+                1 + (self.tau.as_u64() / t.as_u64()) as u32,
+            ));
         }
         Ok(state.measure_and_replace(key, |tat| {
-            let tat = tat.unwrap_or_else(|| self.starting_state(t0));
+            let tat = tat.unwrap_or(t0);
             let earliest_time = (tat + additional_weight).saturating_sub(tau);
             if t0 < earliest_time {
                 Err(MW::disallow(

governor/src/middleware.rs
@@ -74,7 +74,9 @@ pub struct StateSnapshot {
     /// The "weight" of a single packet in units of time.
     t: Nanos,
 
-    /// The "burst capacity" of the bucket.
+    /// The "tolerance" of the bucket.
+    ///
+    /// The total "burst capacity" of the bucket is `t + tau`.
     tau: Nanos,
 
     /// The time at which the measurement was taken.
@@ -106,10 +108,10 @@ impl StateSnapshot {
     /// If this state snapshot is based on a negative rate limiting
     /// outcome, this method returns 0.
     pub fn remaining_burst_capacity(&self) -> u32 {
-        let t0 = self.time_of_measurement + self.t;
+        let t0 = self.time_of_measurement;
         (cmp::min(
-            (t0 + self.tau).saturating_sub(self.tat).as_u64(),
-            self.tau.as_u64(),
+            (t0 + self.tau + self.t).saturating_sub(self.tat).as_u64(),
+            (self.tau + self.t).as_u64(),
         ) / self.t.as_u64()) as u32
     }
 }

governor/src/state/keyed.rs
@@ -182,7 +182,7 @@ where
         // arrival time is larger than a starting state for the bucket gets to stay, everything
         // else (that's indistinguishable from a starting state) goes.
         let now = self.clock.now();
-        let drop_below = now.duration_since(self.start);
+        let drop_below = now.duration_since(self.start).saturating_sub(self.gcra.t());
 
         self.state.retain_recent(drop_below);
     }

commit d34c0c8f822b9b62f89739d111bfb2d996a62176

Rename function parameter to match its purpose
The second GCRA parameter is the tolerance `tau`. The capacity of the
bucket is `t + tau`.

The `tau` parameter was accidentally renamed to `capacity` in
273f77b16db6 as left over changes from a previous experiment.

Fixes: 273f77b16db6 ("Differentiate between GCRA burst capacity and tolerance")

governor/src/quota.rs
@@ -190,14 +190,14 @@ impl Quota {
     /// This is useful mainly for [`crate::middleware::RateLimitingMiddleware`]
     /// where custom code may want to construct information based on
     /// the amount of burst balance remaining.
-    pub(crate) fn from_gcra_parameters(t: Nanos, capacity: Nanos) -> Quota {
+    pub(crate) fn from_gcra_parameters(t: Nanos, tau: Nanos) -> Quota {
         // Safety assurance: by construction, the computed value is bounded by
         // one at the lower.
         //
         // The casts may look a little sketch, but they're constructed from
         // parameters that came from the crate exactly like that.
         let max_burst =
-            unsafe { NonZeroU32::new_unchecked(1 + (capacity.as_u64() / t.as_u64()) as u32) };
+            unsafe { NonZeroU32::new_unchecked(1 + (tau.as_u64() / t.as_u64()) as u32) };
         let replenish_1_per = t.into();
         Quota {
             max_burst,